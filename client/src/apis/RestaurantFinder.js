5b459224b366c4115b70ed9e284e11007a76b885
late-fall <shanwnsolee@gmail.com>
2023-08-24 20:49:55 -0400

    deploy

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -8,7 +8,7 @@ import axios from "axios"
 
 //const baseURL = "http://localhost:3001/api/v1/restaurants"
 
-const baseURL = process.env.NODE_ENV === 'production' ? "api/v1/restaurants" : "http://localhost:3001/api/v1/restaurants"
+const baseURL = process.env.NODE_ENV === 'production' ? "/api/v1/restaurants" : "http://localhost:3001/api/v1/restaurants"
 
 export default axios.create({
     baseURL: baseURL
